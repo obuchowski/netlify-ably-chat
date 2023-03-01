7f73b283fc03fe6f56ea9c179632a770a418ea35
Evgeni Obukhovski <eobuhovsky@gmail.com>
2023-03-01 11:54:32 +0100

    first commit

---
+++ netlify/edge-functions/chat/chat.ts
@@ -0,0 +1,21 @@
+import type { Context, Request } from "netlify:edge"
+// import * as Ably from "https://cdn.ably.com/lib/ably.min-1.js"
+import * as Ably from "npm:ably/promises"
+
+export default async (request: Request, context: Context) => {
+    const ablyApiKey = Deno.env.get('ABLY_API_KEY')
+    if (!ablyApiKey) {
+        return new Response('Missing API_KEY environment variable.', {
+            status: 500,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    }
+
+    const url = new URL(request.url)
+    const clientId = url.searchParams.get('client') || Deno.env.get('DEFAULT_CLIENT_ID') || 'NO_CLIENT_ID'
+    console.log(clientId)
+    const client = new Ably.Rest(ablyApiKey)
+    const tokenRequestData = await client.auth.createTokenRequest({ clientId: clientId })
+
+    return Response.json(tokenRequestData)
+}
